99b83de2748665e41ca861fab64122f6bf8a09e0
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-05-28 17:23:55 +0530

    you have to love this 🔥

--- pages/LandingPage.tsx
+++ pages/LandingPage.tsx
@@ -13,61 +13,43 @@ export default function LandingPage() {
     };
 
     return (
-        <div
-            className="hero min-h-screen"
-            style={{
-                backgroundImage: `url("/images/stock/photo-1507358522600-9f71e620c44e.jpg")`,
-            }}
-        >
-            <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-md">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
+        // ALTERNATE LOGIN FORM:
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-7xl font-bold">
+                        <span className="animate-fire">Brain</span>
+                        <span className="animate-fire">Blitz</span>
+                    </h1>
+
+                    <br></br>
+                    <h1 className="text-4xl font-bold">Login now!</h1>
                     <br></br>
 
-                    {/* <p className="mb-5">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                        assumenda excepturi exercitationem quasi. In deleniti
-                        eaque aut repudiandae et a id nisi.
-                    </p> */}
+                    <p className="py-6 text-2xl font-bold leading-8">
+                        <span className="text-blue-400 animate-pulse">
+                            Unleash Your Mind's Potential
+                        </span>{" "}
+                        with Brain Blitz: The
+                        <span className="text-red-400 animate-pulse">
+                            {" "}
+                            Ultimate
+                        </span>{" "}
+                        Quiz Experience!
+                    </p>
+                </div>
 
-                    <button className="btn btn-primary" onClick={performSignIn}>
-                        Login
-                    </button>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <button
+                            className="btn btn-primary"
+                            onClick={performSignIn}
+                        >
+                            Login
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
-
-        // ALTERNATE LOGIN FORM:
-        // <div className="hero min-h-screen bg-base-200">
-        // <div className="hero-content flex-col lg:flex-row-reverse">
-        //   <div className="text-center lg:text-left">
-        //     <h1 className="text-5xl font-bold">Login now!</h1>
-        //     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-        //   </div>
-        //   <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-        //     <div className="card-body">
-        //       <div className="form-control">
-        //         <label className="label">
-        //           <span className="label-text">Email</span>
-        //         </label>
-        //         <input type="text" placeholder="email" className="input input-bordered" />
-        //       </div>
-        //       <div className="form-control">
-        //         <label className="label">
-        //           <span className="label-text">Password</span>
-        //         </label>
-        //         <input type="text" placeholder="password" className="input input-bordered" />
-        //         <label className="label">
-        //           <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-        //         </label>
-        //       </div>
-        //       <div className="form-control mt-6">
-        //         <button className="btn btn-primary">Login</button>
-        //       </div>
-        //     </div>
-        //   </div>
-        // </div>
-        // </div>
     );
 }
