a64bf6d34b9f69861f256b22d9c53bf20da38f3c
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-05-28 16:07:58 +0530

    centered some things

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,14 @@ import Head from "next/head";
 import LoginBtn from "./components/LoginBtn";
 
 export default function Home() {
-	return (
-		<div>
-			<Head>
-				<title>Create Next App</title>
-			</Head>
-			<div className="flex w-64 justify-center flex-col">
-				<LoginBtn />
-			</div>
-		</div>
-	);
+    return (
+        <div className="center">
+        	<Head>
+        		<title>Create Next App</title>
+        	</Head>
+        	<div className="flex w-64 justify-center flex-col">
+        		<LoginBtn />
+        	</div>
+        </div>
+    );
 }
