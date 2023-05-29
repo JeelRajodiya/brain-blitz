61b4ee746f17de23dbfe6fa9550922470e81793f
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-05-29 14:21:27 +0530

    fix

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoClient } from "mongodb";
-import { access } from "fs";
 
 const uri = process.env.MONGO_URI;
 
