3aebeb0a936251d565b081f972d647fd2ab947d1
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-05-29 23:13:54 +0530

    feature of click the avatar multiple times

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -1,80 +1,25 @@
 import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useState } from "react";
-
-function QuizForm() {
-    const [showForm, setShowForm] = useState(false);
-
-    const toggleForm = () => {
-        setShowForm(!showForm);
-    };
-
-    return (
-        <div>
-            <div
-                className="btn-group button-group"
-                style={{ marginTop: "40px" }}
-            >
-                <button className="btn btn-modified join">Join Quiz</button>
-                <button
-                    className="btn btn-modified create"
-                    onClick={toggleForm}
-                >
-                    Create Quiz
-                </button>
-            </div>
-            {showForm && (
-                <div className="fixed inset-0 flex items-center justify-center">
-                    <div className="card card-compact w-52 bg-base-100 shadow">
-                        <div className="card-header">
-                            <div className="flex justify-end">
-                                <button
-                                    className="btn btn-ghost"
-                                    onClick={toggleForm}
-                                >
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="h-5 w-5"
-                                        viewBox="0 0 20 20"
-                                        fill="currentColor"
-                                    >
-                                        <path
-                                            fillRule="evenodd"
-                                            d="M15.293 4.293a1 1 0 010 1.414L11.414 10l3.879 3.879a1 1 0 01-1.414 1.414L10 11.414l-3.879 3.879a1 1 0 01-1.414-1.414L8.586 10 4.707 6.121a1 1 0 011.414-1.414L10 8.586l3.879-3.879a1 1 0 011.414 0z"
-                                            clipRule="evenodd"
-                                        />
-                                    </svg>
-                                </button>
-                            </div>
-                        </div>
-                        <div className="card-body">
-                            <h2>Create Quiz</h2>
-                            <form>
-                                {/* Form inputs and elements go here */}
-                            </form>
-                            <button
-                                className="btn btn-primary btn-block"
-                                onClick={toggleForm}
-                            >
-                                Start Making Quiz
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            )}
-        </div>
-    );
-}
+import QuizForm from "./components/QuizForm";
 
 export default function Dashboard() {
     const session = useSession();
     const router = useRouter();
+
     if (session.status == "unauthenticated") {
         return router.push("/LandingPage");
     }
     const performSignOut = () => {
         signOut({ callbackUrl: "/LandingPage" });
     };
+
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown);
+    };
+
     return (
         <div className="flex justify-center flex-col center">
             {/* menu bar */}
@@ -86,47 +31,15 @@ export default function Dashboard() {
                     </div>
                 </div>
                 <div className="flex-none">
-                    <div className="dropdown dropdown-end">
-                        {/* <label
-                            tabIndex={0}
-                            className="btn btn-ghost btn-circle"
-                        >
-                            <div className="indicator">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="h-5 w-5"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                ></svg>
-                                <span className="badge badge-sm indicator-item">
-                                    8
-                                </span>
-                            </div>
-                        </label> */}
-                        {/* <div
-                            tabIndex={0}
-                            className="mt-3 card card-compact dropdown-content w-52 bg-base-200 shadow-md"
-                        >
-                            <div className="card-body">
-                                <span className="font-bold text-lg">
-                                    8 Items
-                                </span>
-                                <span className="text-info">
-                                    Subtotal: $999
-                                </span>
-                                <div className="card-actions">
-                                    <button className="btn btn-primary btn-block">
-                                        View cart
-                                    </button>
-                                </div>
-                            </div>
-                        </div> */}
-                    </div>
-                    <div className="dropdown dropdown-end relative z-50">
+                    <div
+                        className={`dropdown dropdown-end ${
+                            showDropdown ? "dropdown-active" : ""
+                        }`}
+                    >
                         <label
                             tabIndex={0}
                             className="btn btn-ghost btn-circle avatar"
+                            onClick={toggleDropdown}
                         >
                             {/* IMAGE ATTRIBUTE */}
                             <div className="w-24 mask mask-squircle">
@@ -138,40 +51,27 @@ export default function Dashboard() {
                                 )}
                             </div>
                         </label>
-                        <ul className="menu menu-compact dropdown-content mt-8 p-2 shadow bg-base-200 rounded-box w-52">
-                            <li>
-                                <a className="justify-between">
-                                    Profile
-                                    <span className="badge">New</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a>Settings</a>
-                            </li>
-                            <li onClick={performSignOut}>
-                                <a>Logout</a>
-                            </li>
-                        </ul>
+                        {showDropdown && (
+                            <ul className="menu menu-compact dropdown-content mt-8 p-2 shadow bg-base-200 rounded-box w-52">
+                                <li>
+                                    <a className="justify-between">
+                                        Profile
+                                        <span className="badge">New</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a>Settings</a>
+                                </li>
+                                <li onClick={performSignOut}>
+                                    <a>Logout</a>
+                                </li>
+                            </ul>
+                        )}
                     </div>
                 </div>
             </div>
 
-            {/* button group */}
-            {/* <div
-                className="btn-group button-group"
-                style={{ marginTop: "80px" }}
-            >
-                <button className="btn btn-modified join">Join Quiz</button>
-                <button className="btn btn-modified create">
-                    Create Quiz
-                </button>
-            </div> */}
-
             <QuizForm></QuizForm>
-
-            {/* <button className="btn w-64 al" onClick={performSignOut}>
-                Log out
-            </button> */}
         </div>
     );
 }
