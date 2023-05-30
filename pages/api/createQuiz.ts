7bd2fc17ead69d68b67f71cd95f83ca9734f564f
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-05-30 17:41:23 +0530

    Create Quiz works perfectly

--- pages/api/createQuiz.ts
+++ pages/api/createQuiz.ts
@@ -1,9 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getDB } from "../../util/DB";
+import { getDB, getUser } from "../../util/DB";
 import type { QuizCol } from "../../util/DB";
 import { uuid } from "uuidv4";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./auth/[...nextauth]";
+import { get } from "http";
 
 export default async function createQuiz(
     req: NextApiRequest,
@@ -15,11 +16,17 @@ export default async function createQuiz(
     // return res.send("Hello");
 }
 
-function POST(req: NextApiRequest, res: NextApiResponse) {
+async function POST(req: NextApiRequest, res: NextApiResponse) {
     const quiz: QuizCol = req.body;
     console.log(quiz);
-    // const session = getServerSession(req, res, authOptions);
-    // console.log(session);
-    // return res.status(200);
-    return res.send("Hello");
+    const session = await getServerSession(req, res, authOptions);
+    const user = getUser(session.user.email);
+    if (!user) {
+        return res.status(401).send("Unauthorized");
+    }
+    quiz.userId = (await user).id;
+    quiz.id = uuid();
+    const db = await getDB();
+    await db.collection<QuizCol>("quizzes").insertOne(quiz);
+    return res.status(200).send("OK");
 }
--- util/DB.ts
+++ util/DB.ts
@@ -1,4 +1,5 @@
 import { MongoClient } from "mongodb";
+import { User } from "next-auth";
 export type QuizCol = {
     title: string;
     userId: string; // the user's id who created the quiz
@@ -10,8 +11,27 @@ export type QuizCol = {
     markForCorrect: number; // default +4
     markForIncorrect: number; // default -1
 };
+
+type UserCol = {
+    email: string;
+    id: string;
+    name: string;
+    dateOfJoining: Date;
+    sessions: string;
+    image: string;
+    accessToken: string;
+    idToken: string;
+    createdAt: Date;
+};
+
 export function getDB() {
     const uri = process.env.MONGO_URI;
     const client = new MongoClient(uri);
     return client.db("brain-blitz");
+}
+
+export async function getUser(email: string) {
+    const db = await getDB();
+    const user = await db.collection<UserCol>("users").findOne({ email });
+    return user;
 }
