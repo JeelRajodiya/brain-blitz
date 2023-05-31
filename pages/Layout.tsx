0a3a01cf8237dbc06a9113a55fbc36d0aeed82f7
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-05-31 14:01:45 +0530

    fixed (not really)

---
+++ pages/Layout.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+
+export default function Layout({ children }) {
+	const session = useSession();
+	const router = useRouter();
+
+	if (session.status == "unauthenticated") {
+		return router.push("/LandingPage");
+	}
+	const performSignOut = () => {
+		signOut({ callbackUrl: "/LandingPage" });
+	};
+	return (
+		<div className="flex justify-center flex-col center">
+			<div className="navbar bg-base-200 shadow-md p-2 mx-2 mb-4">
+				<div className="flex-1">
+					<div className="center">
+						<h1 className="text-5xl font-bold">Dashboard</h1>
+					</div>
+				</div>
+				<div className="flex-none">
+					<div className="dropdown dropdown-end">
+						<label
+							tabIndex={0}
+							className="btn btn-ghost btn-circle avatar"
+						>
+							<div className="w-24 mask mask-squircle">
+								{session.data && session.data.user?.image && (
+									<img
+										src={session.data.user.image}
+										alt="User Avatar"
+									/>
+								)}
+							</div>
+						</label>
+						<ul
+							tabIndex={0}
+							className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+						>
+							<li>
+								<a>profile</a>
+							</li>
+							<li>
+								<a>settings</a>
+							</li>
+							<li>
+								<a onClick={performSignOut}>logout</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			{children}
+		</div>
+	);
+}
