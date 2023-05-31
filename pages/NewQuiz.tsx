1d3cd2efb2c4ed02334f58ac8b81d3b24db37551
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-05-31 17:46:24 +0530

    UI fix

--- pages/NewQuiz.tsx
+++ pages/NewQuiz.tsx
@@ -8,104 +8,102 @@ export default function NewQuiz() {
 		setQuizName(event.target.value);
 	};
 
-	const handleClick = () => {
-		if (quizName) {
-			// Perform the desired action when the button is clicked and quizName has data
-			console.log("Button clicked!");
-		}
-	};
 	return (
 		// @ts-ignore
 		<Layout pageTitle={"New Quiz"}>
-			<div className="flex flex-col ">
-				<div className="flex justify-between">
-					<label className="label m-5">
-						<span className=" txtf label-text">Quiz Name</span>
-					</label>
-					<input
-						type="text"
-						placeholder="My awesome quiz"
-						className="txtin m-5 input input-bordered"
-						value={quizName}
-						onChange={handleQuizNameChange}
-					/>
+			<div className="flex flex-col">
+				<div className="flex flex-col ">
+					<div className="flex justify-between">
+						<label className="label m-5">
+							<span className=" txtf label-text">Quiz Name</span>
+						</label>
+						<input
+							type="text"
+							placeholder="My awesome quiz"
+							className="txtin m-5 input input-bordered"
+							value={quizName}
+							onChange={handleQuizNameChange}
+						/>
+					</div>
+					<div className="flex justify-between">
+						<label className="label m-5">
+							<span className=" txtf label-text">
+								Enable Difficultly Tags{" "}
+							</span>
+						</label>
+						<input
+							type="checkbox"
+							className="toggle toggle-md toggle-accent m-5"
+						/>
+					</div>
+					<div className="flex justify-between">
+						<label className="label m-5">
+							<span className=" txtf label-text">
+								Enable Polls{" "}
+							</span>
+						</label>
+						<input
+							type="checkbox"
+							className="toggle toggle-md toggle-accent m-5"
+						/>
+					</div>
+					<div className="flex justify-between">
+						<label className="label m-5">
+							<span className=" txtf label-text">
+								Jump Questions{" "}
+							</span>
+						</label>
+						<input
+							type="checkbox"
+							className="toggle toggle-md toggle-accent m-5"
+						/>
+					</div>
+					<div className="flex justify-between">
+						<label className="label m-5">
+							<span className=" txtf label-text">
+								Time for a question{" "}
+							</span>
+						</label>
+						<input
+							type="number"
+							placeholder="10"
+							className=" m-5 txtin input input-bordered"
+						/>
+					</div>
+					<div className="flex justify-between">
+						<label className="label m-5">
+							<span className=" txtf label-text">
+								Mark for correct{" "}
+							</span>
+						</label>
+						<input
+							type="number"
+							placeholder="5"
+							className=" m-5 input txtin input-bordered"
+						/>
+					</div>
+					<div className="flex justify-between">
+						<label className="label m-5">
+							<span className=" txtf label-text">
+								Mark for incorrect{" "}
+							</span>
+						</label>
+						<input
+							type="number"
+							placeholder="-1"
+							className=" m-5 input txtin input-bordered"
+						/>
+					</div>
 				</div>
-				<div className="flex justify-between">
-					<label className="label m-5">
-						<span className=" txtf label-text">
-							Enable Difficultly Tags{" "}
-						</span>
-					</label>
-					<input
-						type="checkbox"
-						className="toggle toggle-md toggle-accent m-5"
-					/>
-				</div>
-				<div className="flex justify-between">
-					<label className="label m-5">
-						<span className=" txtf label-text">Enable Polls </span>
-					</label>
-					<input
-						type="checkbox"
-						className="toggle toggle-md toggle-accent m-5"
-					/>
-				</div>
-				<div className="flex justify-between">
-					<label className="label m-5">
-						<span className=" txtf label-text">
-							Jump Questions{" "}
-						</span>
-					</label>
-					<input
-						type="checkbox"
-						className="toggle toggle-md toggle-accent m-5"
-					/>
-				</div>
-				<div className="flex justify-between">
-					<label className="label m-5">
-						<span className=" txtf label-text">
-							Time for a question{" "}
-						</span>
-					</label>
-					<input
-						type="number"
-						placeholder="10"
-						className=" m-5 txtin input input-bordered"
-					/>
-				</div>
-				<div className="flex justify-between">
-					<label className="label m-5">
-						<span className=" txtf label-text">
-							Mark for correct{" "}
-						</span>
-					</label>
-					<input
-						type="number"
-						placeholder="5"
-						className=" m-5 input txtin input-bordered"
-					/>
-				</div>
-				<div className="flex justify-between">
-					<label className="label m-5">
-						<span className=" txtf label-text">
-							Mark for incorrect{" "}
-						</span>
-					</label>
-					<input
-						type="number"
-						placeholder="-1"
-						className=" m-5 input txtin input-bordered"
-					/>
-				</div>
-			</div>
 
-			<div className="bb">
-				<button
-					className="btn btn-primary btn-active w-48 h-20"
-					disabled={!quizName}
-				>
-					{!quizName ? "Click me" : "Fuck me"}
-				</button>
+				<div className="bb  justify-end">
+					<button
+						className="btn btn-primary btn-active w-48 h-20"
+						disabled={!quizName}
+					>
+						Click me
+					</button>
+				</div>
 			</div>
 		</Layout>
 	);
