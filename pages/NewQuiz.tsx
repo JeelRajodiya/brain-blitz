542770859272d8d45763ad690066c59ba5551bfc
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-05-31 16:28:16 +0530

    added the button in a flashy way, not in pos yet

--- pages/NewQuiz.tsx
+++ pages/NewQuiz.tsx
@@ -2,85 +2,105 @@ import Layout from "./Layout";
 import * as React from "react";
 
 export default function NewQuiz() {
-	// "title": "Sample Quiz",
+	const [quizName, setQuizName] = React.useState("");
 
-	// "difficultyTags": true,
-	// "isPoll": false,
-	// "jumpQuestions": true,
-	// "timeForAQuestion": 10,
-	// "markForCorrect": 5,
-	// "markForIncorrect": -1
+	const handleQuizNameChange = (event) => {
+		setQuizName(event.target.value);
+	};
+
+	const handleClick = () => {
+		if (quizName) {
+			// Perform the desired action when the button is clicked and quizName has data
+			console.log("Button clicked!");
+		}
+	};
 	return (
 		// @ts-ignore
 		<Layout pageTitle={"New Quiz"}>
-			<div className="flex flex-col" >
+			<div className="flex flex-col ">
 				<div className="flex justify-between">
 					<label className="label m-5">
-						<span className="label-text">Quiz Name</span>
+						<span className=" txtf label-text">Quiz Name</span>
 					</label>
 					<input
 						type="text"
 						placeholder="My awesome quiz"
-						className=" m-5 input input-bordered"
+						className="txtin m-5 input input-bordered"
+						value={quizName}
+						onChange={handleQuizNameChange}
 					/>
 				</div>
 				<div className="flex justify-between">
 					<label className="label m-5">
-						<span className="label-text">Difficultly Tags </span>
+						<span className=" txtf label-text">
+							Enable Difficultly Tags{" "}
+						</span>
 					</label>
 					<input
 						type="checkbox"
-						className="toggle toggle-accent m-5"
+						className="toggle toggle-md toggle-accent m-5"
 					/>
 				</div>
 				<div className="flex justify-between">
 					<label className="label m-5">
-						<span className="label-text">Is Poll </span>
+						<span className=" txtf label-text">Enable Polls </span>
 					</label>
 					<input
 						type="checkbox"
-						className="toggle toggle-accent m-5"
+						className="toggle toggle-md toggle-accent m-5"
 					/>
 				</div>
 				<div className="flex justify-between">
 					<label className="label m-5">
-						<span className="label-text">Jump Questions </span>
+						<span className=" txtf label-text">
+							Jump Questions{" "}
+						</span>
 					</label>
 					<input
 						type="checkbox"
-						className="toggle toggle-accent m-5"
+						className="toggle toggle-md toggle-accent m-5"
 					/>
 				</div>
 				<div className="flex justify-between">
 					<label className="label m-5">
-						<span className="label-text">Time for a question </span>
+						<span className=" txtf label-text">
+							Time for a question{" "}
+						</span>
 					</label>
 					<input
 						type="number"
 						placeholder="10"
-						className=" m-5 input input-bordered"
+						className=" m-5 txtin input input-bordered"
 					/>
 				</div>
 				<div className="flex justify-between">
 					<label className="label m-5">
-						<span className="label-text">Mark for correct </span>
+						<span className=" txtf label-text">
+							Mark for correct{" "}
+						</span>
 					</label>
 					<input
 						type="number"
 						placeholder="5"
-						className=" m-5 input input-bordered"
+						className=" m-5 input txtin input-bordered"
 					/>
 				</div>
 				<div className="flex justify-between">
 					<label className="label m-5">
-						<span className="label-text">Mark for incorrect </span>
+						<span className=" txtf label-text">
+							Mark for incorrect{" "}
+						</span>
 					</label>
 					<input
 						type="number"
 						placeholder="-1"
-						className=" m-5 input input-bordered"
+						className=" m-5 input txtin input-bordered"
 					/>
 				</div>
+
+				<button className="btn mt-7" disabled={!quizName}>
+					Click Me
+				</button>
 			</div>
 		</Layout>
 	);
