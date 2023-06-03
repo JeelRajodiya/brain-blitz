22130f01d8131ef57862d03a467638d375d049dc
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-06-03 15:27:48 +0530

    😊😊 it is working (kinda)

--- pages/NewQuiz/Questions.tsx
+++ pages/NewQuiz/Questions.tsx
@@ -6,7 +6,6 @@ import { useState } from "react";
 
 const IndexEntry = ({ srNo, name }) => (
   <tr className="flex justify-between">
-    <td>{srNo}</td>
     <td>{name}</td>
     <td>
       <DeleteButton />
@@ -14,11 +13,35 @@ const IndexEntry = ({ srNo, name }) => (
   </tr>
 );
 
-function Option({ isPoll }: { isPoll: boolean }) {
+function Option({
+  isPoll,
+  question,
+  setQuestion,
+  index,
+}: {
+  isPoll: boolean;
+  question: Question;
+  setQuestion: Function;
+  index: number;
+}) {
+  console.log(index);
   const checkBox = (
     <td className="checkBox">
       <label>
-        <input type="checkbox" className="checkbox" />
+        <input
+          type="checkbox"
+          className="checkbox"
+          checked={question.correctOption === index}
+          onChange={(e) => {
+            const newQuestion = structuredClone(question);
+            if (e.target.checked) {
+              newQuestion.correctOption = index;
+            } else {
+              newQuestion.correctOption = -1;
+            }
+            setQuestion(newQuestion);
+          }}
+        />
       </label>
     </td>
   );
@@ -31,6 +54,12 @@ function Option({ isPoll }: { isPoll: boolean }) {
           type="text"
           placeholder="Type option here"
           className="input input-bordered input-primary w-full"
+          value={question.options[index]}
+          onChange={(e) => {
+            const newQuestion = structuredClone(question);
+            newQuestion.options[index] = e.target.value;
+            setQuestion(newQuestion);
+          }}
         />
       </td>
     </tr>
@@ -87,7 +116,12 @@ function DifficultlyTag() {
     </>
   );
 }
-
+type Question = {
+  question: string;
+  options: string[];
+  correctOption: number;
+  difficulty?: number;
+};
 export default function Questions() {
   const router = useRouter();
   const {
@@ -99,6 +133,12 @@ export default function Questions() {
     markForCorrect,
     markForIncorrect,
   } = router.query;
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [question, setQuestion] = useState<Question>({
+    question: "",
+    options: [],
+    correctOption: 0,
+  });
   return (
     <>
       {/* @ts-ignore */}
@@ -119,16 +159,15 @@ export default function Questions() {
             <table className="table table-zebra">
               <thead>
                 <tr className="grid grid-cols-3">
-                  <th className="justify-self-start">Sr.</th>
                   <th className="justify-self-center">Name</th>
                   <th className="justify-self-end">Action</th>
                 </tr>
               </thead>
 
               <tbody>
-                <IndexEntry srNo={1} name="Question 1" />
-                <IndexEntry srNo={2} name="Question 2" />
-                <IndexEntry srNo={3} name="Question 3" />
+                {questions.map((i, n) => (
+                  <IndexEntry srNo={n} name={`Question ${n}`} />
+                ))}
               </tbody>
             </table>
             <button className="btn btn-success mt-4 mb-4 w-full btn-wide text-center">
@@ -142,16 +181,29 @@ export default function Questions() {
           <div className="w-3/5">
             <div className="flex flex-row justify-between items-center">
               <h1 className="text-2xl mb-4">Question:</h1>
-              <button className="btn mb-4 btn-outline btn-success">
+              <button
+                className="btn mb-4 btn-outline btn-success"
+                onClick={() => {
+                  const newQuestions = structuredClone(questions);
+                  newQuestions.push(question);
+                  setQuestions(newQuestions);
+                }}
+              >
                 Save Question
               </button>
             </div>
 
             <div>
-              <textarea
-                placeholder="Type here"
+              <input
+                placeholder="Who is the president of UK?"
                 className="input input-bordered input-primary w-full resize-y max-h-36"
-              ></textarea>
+                value={question.question}
+                onChange={(e) => {
+                  const newQuestion = structuredClone(question);
+                  newQuestion.question = e.target.value;
+                  setQuestion(newQuestion);
+                }}
+              ></input>
             </div>
 
             {/* MCQ details table */}
@@ -168,6 +220,9 @@ export default function Questions() {
                     return (
                       <Option
                         key={i}
+                        index={i}
+                        question={question}
+                        setQuestion={setQuestion}
                         isPoll={isPolls == "true" ? true : false}
                       />
                     );
