889af73283e904ca5352ec3acda0ddceea978c1a
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-06-03 19:17:03 +0530

    daisyUI version 3.0.1

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -2,13 +2,13 @@ import QuizForm from "./components/QuizActions";
 import * as React from "react";
 import Layout from "./Layout";
 export default function Dashboard() {
-	return (
-		//@ts-ignore
-		<Layout pageTitle={"Dashboard"}>
-			<QuizForm />
-			<button className="btn btn-wide btn-modified btn-primary mt-5">
-				View Previous Reports
-			</button>
-		</Layout>
-	);
+  return (
+    //@ts-ignore
+    <Layout pageTitle={"Dashboard"}>
+      <QuizForm />
+      <button className="btn btn-wide btn-modified btn-primary mt-5">
+        View Previous Reports
+      </button>
+    </Layout>
+  );
 }
