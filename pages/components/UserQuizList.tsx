7979383617c6b0391ce88ecaf1ef51360d214c0e
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-06-07 17:52:12 +0530

    flex works

--- pages/components/UserQuizList.tsx
+++ pages/components/UserQuizList.tsx
@@ -7,18 +7,18 @@ type QuizList = {
 };
 export default function UserQuizList({ quizList }: { quizList: QuizList[] }) {
   return (
-    <div className="gird grid-cols-1 grid-flow-row">
-      <div className="grid grid-cols-3  ">
-        <div className="col-span-1">Title</div>
-        <div className="col-span-2 ">Code</div>
-        <div className="col-span-3 ">Created At</div>
+    <div className="flex flex-col">
+      <div className="  flex flex-row justify-between">
+        <div>Title</div>
+        <div>Code</div>
+        <div>Created At</div>
       </div>
       {quizList.map((quiz, index) => {
         return (
-          <div className="grid col-span-3 row-span-1" key={quiz.id}>
-            <div className="col-span-1">{quiz.title}</div>
-            <div className="col-span-2">{quiz.code}</div>
-            <div className="col-span-3">
+          <div className="flex flex-row justify-between" key={quiz.id}>
+            <div>{quiz.title}</div>
+            <div>{quiz.code}</div>
+            <div>
               {new Date(quiz.createdAt).toLocaleDateString(undefined, {
                 year: "numeric", // Display the full year (e.g., "2023")
                 month: "long", // Display the full month name (e.g., "January")
