46f9f6729107e32d6f857c4995c5fbe0497a7d15
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-06-09 16:08:42 +0530

    basic skeleton done for now...

--- pages/components/UserQuizListSkeleton.tsx
+++ pages/components/UserQuizListSkeleton.tsx
@@ -1,3 +1,60 @@
+import styles from "./UserQuizList.module.css";
+import classNames from "classnames";
+
 export default function UserQuizListSkeleton() {
-  return <div className="flex">Loading Skeleton</div>;
+  return (
+    <div className="flex mt-20 items-center flex-col">
+      <div
+        className={classNames(`self-center text-xl`, styles.skeletonText)}
+      ></div>
+      <div className={classNames(styles.tableHeading, styles.skeleton)}>
+        <div className="p-10"></div>
+        <div className="p-10"></div>
+        <div className="p-10"></div>
+        <div className="p-10"></div>
+      </div>
+
+      <div
+        className={classNames(
+          `flex flex-row  justify-between items-center `,
+          styles.displayTable
+        )}
+      >
+        <div className={`p-2 ${styles.skeletonText}`}></div>
+        <div
+          className={`p-2 tooltip tooltip-top tooltip-info cursor-pointer ${styles.skeletonText}`}
+        ></div>
+        <div className={`p-2 text-xs ${styles.skeletonText}`}></div>
+        {/* Add a button here */}
+        <div
+          className={`dropdown dropdown-hover justify-center flex dropdown-bottom ${styles.skeletonText}`}
+        >
+          <label
+            tabIndex={0}
+            className={`hover:bg-base-200 p-1 rounded-md ${styles.skeletonText}`}
+          >
+            <div></div>
+          </label>
+          <ul
+            tabIndex={0}
+            className={`dropdown-content shadow bg-base-200 rounded-box z-50 p-2 ${styles.skeletonText}`}
+          >
+            <li>
+              <button className={`btn btn-xs btn-ghost ${styles.skeletonText}`}>
+                {" "}
+                Edit
+              </button>
+            </li>
+            <li>
+              <button className={`btn btn-xs btn-ghost ${styles.skeletonText}`}>
+                {" "}
+                Delete
+              </button>
+            </li>
+          </ul>
+        </div>
+        
+      </div>
+    </div>
+  );
 }
