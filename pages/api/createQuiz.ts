2daecb06405bc119ebae08b7f04f1248fb9e812c
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-06-09 13:35:54 +0530

    added Delete method

--- pages/api/createQuiz.ts
+++ pages/api/createQuiz.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getDB, getUser } from "../../util/DB";
+import { QuestionCol, getDB, getUser } from "../../util/DB";
 import type { QuizCol } from "../../util/DB";
 import { uuid } from "uuidv4";
 import { getServerSession } from "next-auth";
@@ -19,6 +19,8 @@ export default async function createQuiz(
 ) {
   if (req.method === "POST") {
     return POST(req, res);
+  } else if (req.method === "DELETE") {
+    return DELETE(req, res);
   }
   // return res.send("Hello");
 }
@@ -37,4 +39,32 @@ async function POST(req: NextApiRequest, res: NextApiResponse) {
   const db = await getDB();
   await db.collection<QuizCol>("quizzes").insertOne(quiz);
   return res.json({ quizId: quiz.id, code: quiz.code });
+}
+
+async function DELETE(req: NextApiRequest, res: NextApiResponse) {
+  const quiz: QuizCol = req.body;
+  const session = await getServerSession(req, res, authOptions);
+  const user = getUser(session.user.email);
+  if (!user) {
+    return res.status(401).send("Unauthorized");
+  }
+  quiz.userId = (await user).id;
+  quiz.id = uuid();
+  quiz.code = generateUID();
+  quiz.createdAt = new Date();
+  const db = await getDB();
+  const QuizDel = await db.collection<QuizCol>("quizzes").deleteOne(quiz);
+  const QueDel = await db
+    .collection<QuestionCol>("questions")
+    .deleteMany({ quizId: quiz.id });
+
+  if (QuizDel.acknowledged && QueDel.acknowledged) {
+    return res.json({ quizId: quiz.id, code: quiz.code });
+  } else if (!QuizDel.acknowledged && !QueDel.acknowledged) {
+    return res.status(500).send("Quiz and Questions not deleted");
+  } else if (!QuizDel.acknowledged) {
+    return res.status(500).send("Quiz not deleted");
+  } else if (!QueDel.acknowledged) {
+    return res.status(500).send("Questions not deleted");
+  }
 }
