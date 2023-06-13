8f7b8090880992042c06830fa0cd7d0684975bef
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-06-13 18:49:19 +0530

    added "you have no quiz" thingy

--- pages/components/UserQuizList.tsx
+++ pages/components/UserQuizList.tsx
@@ -49,15 +49,39 @@ async function deleteQuiz(
   }
 }
 
+// add no entries part
+// add a show more button
+
 export default function UserQuizList({ quizList }: { quizList: QuizList[] }) {
   const [tooltipText, setTooltipText] = React.useState("Click to copy");
   const [quizListState, setQuizListState] = React.useState(quizList);
+
+  const entries = quizListState.filter((quiz) => !quiz.isDeleted).length;
+  // console.log(entries); // it works
 
   React.useEffect(() => {
     quizListState.map((quiz, index) => {
       quiz.isDeleted = false;
     });
   }, []);
+
+  if (entries === 0) {
+    return (
+      <div className="m-10 ">
+        <div className="card w-96 ml-6 shadow-xl image-full">
+          <figure>
+            <img src="/icons/doodle.png" />
+          </figure>
+          <div className="card-body">
+            <h3 className="card-title pt-14 justify-center">
+              You have not created any quiz!
+            </h3>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="flex  items-center flex-col mt-10 ">
       <div className="self-center text-xl p-5">Your Quizzes</div>
