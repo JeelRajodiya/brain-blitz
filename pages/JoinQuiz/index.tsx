1e688cdff1ca9ad04b8e071405d93cb050f03aa4
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-06-15 21:36:09 +0530

    starting layout from 0 again

--- pages/JoinQuiz/index.tsx
+++ pages/JoinQuiz/index.tsx
@@ -71,35 +71,7 @@ export default function Questions() {
     <>
       {/* @ts-ignore */}
       <Layout>
-        <div className="flex mt-50">
-          {/* ------------------drawer:------------------ */}
-          <div className="drawer">
-            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col items-center justify-center">
-              {/* Page content here */}
-              <label
-                htmlFor="my-drawer-2"
-                className="btn btn-primary drawer-button lg:hidden"
-              >
-                Open drawer
-              </label>
-            </div>
-            <div className="drawer-side">
-              <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-              <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
-                {/* Sidebar content here */}
-                <li>
-                  <a>Sidebar Item 1</a>
-                </li>
-                <li>
-                  <a>Sidebar Item 2</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          {/* ---------------------------------------- */}
-          <div>Question Area</div>
-        </div>
+        
       </Layout>
     </>
   );
