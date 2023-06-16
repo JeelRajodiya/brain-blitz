b1c60175cf6efd2028c6f30b4fc5529559a1ad10
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-06-16 14:50:28 +0530

    Removed the functions, Exported the uri

--- pages/api/createQuestions.ts
+++ pages/api/createQuestions.ts
@@ -1,34 +1,39 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getDB, getUser } from "../../util/DB";
+import { uri } from "../../util/DB";
 import type { QuizCol, QuestionCol } from "../../util/DB";
 import { uuid } from "uuidv4";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./auth/[...nextauth]";
+import { MongoClient } from "mongodb";
 
 export default async function createQuiz(
-	req: NextApiRequest,
-	res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-	if (req.method === "POST") {
-		return POST(req, res);
-	}
-	// return res.send("Hello");
+  if (req.method === "POST") {
+    return POST(req, res);
+  }
+  // return res.send("Hello");
 }
 
 async function POST(req: NextApiRequest, res: NextApiResponse) {
-	const questions = req.body as QuestionCol[];
-	const session = await getServerSession(req, res, authOptions);
-	const user = getUser(session.user.email);
-	if (!user) {
-		return res.status(401).send("Unauthorized");
-	}
-	// const userId = (await user).id;
-	console.log(typeof questions);
-	questions.forEach((question) => {
-		question.id = uuid();
-	});
+  const questions = req.body as QuestionCol[];
+  const session = await getServerSession(req, res, authOptions);
+  const client = new MongoClient(uri);
+  const db = await client.db("brain-blitz");
+  const user = await db
+    .collection("users")
+    .findOne({ email: session.user.email });
 
-	const db = await getDB();
-	await db.collection<QuestionCol>("questions").insertMany(questions);
-	return res.send("Done");
+  if (!user) {
+    return res.status(401).send("Unauthorized");
+  }
+  // const userId = (await user).id;
+  console.log(typeof questions);
+  questions.forEach((question) => {
+    question.id = uuid();
+  });
+
+  await db.collection<QuestionCol>("questions").insertMany(questions);
+  return res.send("Done");
 }
