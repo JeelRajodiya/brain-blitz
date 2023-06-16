733c3d3ba0412082d15a8d4c7f396c92e172fc71
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-06-16 21:00:17 +0530

    basic layout for the quiz

--- pages/JoinQuiz/index.tsx
+++ pages/JoinQuiz/index.tsx
@@ -66,33 +66,26 @@ export default function Questions() {
     );
   }
 
-  //! PROBLEM: the drawer is not fitting properly
-
   const toggleSidebar = () => {
     setShowSidebar(!ShowSidebar);
   };
-  const questionsBoxes = [];
-  for (let i = 0; i < questions.length; i++) {
-    if (i == activeQuestion - 1) {
-      questionsBoxes.push(
-        <div
-          className={classnames(styles.questionBoxActive, styles.questionBox)}
-        >
-          {i + 1}
-        </div>
-      );
-      continue;
-    }
-    questionsBoxes.push(<div className={styles.questionBox}>{i + 1}</div>);
-  }
 
   return (
     <>
       {/* @ts-ignore */}
       <Layout>
         <div className={styles.wrapper}>
-          <div className={styles.sideBar}>{questionsBoxes}</div>
-          <div className={styles.mainWindow}>Main window</div>
+          {jumpQuestions && <div className={styles.sideBar}>Side Bar</div>}
+          {/* main window */}
+          <div className={styles.mainWindow}>
+            {/* progress bar here */}
+            <div className="navbar rounded-lg m-1 bg-neutral"></div>
+            {/* question body here */}
+            <div className="navbar rounded-lg m-1 bg-neutral">{/*  */}</div>
+            <div className="divider"></div> 
+            {/* options here: */}
+            <div className="navbar rounded-lg m-1 bg-neutral">{/*  */}</div>
+          </div>
         </div>
       </Layout>
     </>
